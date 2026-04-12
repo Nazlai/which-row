2e52a5c23cb415c84c717a059158739c0dad327a
nazlai <joebcrmd@gmail.com>
2026-04-12 19:35:36 +0800

    add implementation to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { Instructions } from "./components/Instructions/Instructions";
+import { Navigation } from "./components/Navigation/Navigation";
+import { FindRow } from "./screens/FindRow/FindRow";
+
 function App() {
   return (
-    <div>
-      <p className="text-emerald-500">app</p>
+    <div className="h-screen bg-neutral-900 text-zinc-100">
+      <Navigation />
+      <Instructions />
+      <FindRow />
     </div>
   );
 }
